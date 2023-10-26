ea52927e58ba04c9fa042654932eb2a1193bfea2
MichaelBMoss <michaelbmoss@gmail.com>
2023-10-26 01:35:25 -0700

    can add to database

--- controllers/flights.js
+++ controllers/flights.js
@@ -16,25 +16,20 @@ module.exports = {
     }
   };
   
-
 function newFlight(req, res) {
   res.render('flights/new', { errorMsg: '' });
 }
 
 async function create(req, res) {
-  // convert nowShowing's checkbox of nothing or "on" to boolean
-  req.body.nowShowing = !!req.body.nowShowing;
-  // remove any whitespace at start and end of cast
-  req.body.cast = req.body.cast.trim();
-  // split cast into an array if it's not an empty string - using a regular expression as a separator
-  if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+  // req.body.nowShowing = !!req.body.nowShowing;
+  // // remove any whitespace at start and end of cast
+  // req.body.cast = req.body.cast.trim();
+  // // split cast into an array if it's not an empty string - using a regular expression as a separator
+  // if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
   try {
     await Flight.create(req.body);
-    // Always redirect after CUDing data
-    // We'll refactor to redirect to the flights index after we implement it
     res.redirect('/flights/index');
   } catch (err) {
-    // Typically some sort of validation error
     console.log(err);
     res.render('flights/new', { errorMsg: err.message });
   }
--- models/flight.js
+++ models/flight.js
@@ -3,13 +3,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    title: String,
-    releaseYear: Number,
-    mpaaRating: String,
-    cast: [String],
-    nowShowing: Boolean,
-  }, {
-    timestamps: true
-  });
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United'],
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    default: 'DEN',
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999,
+  },
+  departs: {
+    type: Date,
+    default: function () {
+      // Calculate the default departure date as 1 year from now
+      const oneYearFromNow = new Date();
+      oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
+      return oneYearFromNow;
+    },
+  },
+}, {
+  timestamps: true
+});
+
 
   module.exports = mongoose.model('Flight', flightSchema);
